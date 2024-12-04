d9538992bbfac57b04516eb2883eeea66d26b320
polykernel <81340136+polykernel@users.noreply.github.com>
2024-12-04 02:05:15 -0500

    feat: add day 04 solution

--- src/day04.zig
+++ src/day04.zig
@@ -10,7 +10,161 @@ const gpa = util.gpa;
 
 const data = @embedFile("data/day04.txt");
 
-pub fn main() !void {}
+pub fn main() !void {
+    const part1_pattern1 = 'X' << 24 | 'M' << 16 | 'A' << 8 | 'S';
+    const part1_pattern2 = 'S' << 24 | 'A' << 16 | 'M' << 8 | 'X';
+
+    const part2_pattern1 = 'M' << 16 | 'A' << 8 | 'S';
+    const part2_pattern2 = 'S' << 16 | 'A' << 8 | 'M';
+
+    var grid = List([]const u8).init(gpa);
+    defer grid.deinit();
+
+    var it = tokenizeAny(u8, data, "\r\n");
+    while (it.next()) |line| {
+        try grid.append(line);
+    }
+
+    var xmas_count1: usize = 0;
+    var xmas_count2: usize = 0;
+
+    const image = try grid.toOwnedSlice();
+
+    const m = image.len;
+    const n = image[0].len;
+
+    var marked_image = try gpa.alloc([]u32, m);
+    defer {
+        for (marked_image) |mrow| {
+            gpa.free(mrow);
+        }
+        gpa.free(marked_image);
+    }
+
+    for (marked_image) |*mrow| {
+        mrow.* = try gpa.alloc(u32, n);
+        @memset(mrow.*, 0);
+    }
+
+    {
+        var i: usize = 0;
+        while (i < m) : (i += 1) {
+            var j: usize = 0;
+            var key: u32 = 0;
+            while (j < n and j < 3) : (j += 1) {
+                key = key << 8 | image[i][j];
+            }
+            while (j < n) : (j += 1) {
+                key = key << 8 | image[i][j];
+                if (key == part1_pattern1 or key == part1_pattern2) {
+                    xmas_count1 += 1;
+                }
+            }
+        }
+    }
+
+    {
+        var j: usize = 0;
+        while (j < n) : (j += 1) {
+            var i: usize = 0;
+            var key: u32 = 0;
+            while (i < m and i < 3) : (i += 1) {
+                key = key << 8 | image[i][j];
+            }
+            while (i < m) : (i += 1) {
+                key = key << 8 | image[i][j];
+                if (key == part1_pattern1 or key == part1_pattern2) {
+                    xmas_count1 += 1;
+                }
+            }
+        }
+    }
+
+    {
+        var k: usize = 0;
+        while (k < m + n - 1) : (k += 1) {
+            var i: usize = if (k < m) k else m - 1;
+            var j: usize = if (k < m) 0 else k - m + 1;
+            const nsteps: usize = if (k < @min(m, n)) k + 1 else k + 1 - (2 * k - (n - 1 + m - 1));
+            var v: usize = 0;
+            var key1: u32 = 0;
+            var key2: u24 = 0;
+            while (v < nsteps and v < 3) : ({
+                i -|= 1;
+                j += 1;
+                v += 1;
+            }) {
+                key1 = key1 << 8 | image[i][j];
+                key2 = key2 << 8 | image[i][j];
+            }
+            if (key2 == part2_pattern1 or key2 == part2_pattern2) {
+                marked_image[i + 2][j - 2] += 1;
+            }
+            while (v < nsteps) : ({
+                i -|= 1;
+                j += 1;
+                v += 1;
+            }) {
+                key1 = key1 << 8 | image[i][j];
+                key2 = key2 << 8 | image[i][j];
+                if (key1 == part1_pattern1 or key1 == part1_pattern2) {
+                    xmas_count1 += 1;
+                }
+                if (key2 == part2_pattern1 or key2 == part2_pattern2) {
+                    marked_image[i + 1][j - 1] += 1;
+                }
+            }
+        }
+    }
+
+    {
+        var k: usize = 0;
+        while (k < m + n - 1) : (k += 1) {
+            var i: usize = if (k < m) k else m - 1;
+            var j: usize = if (k < m) n - 1 else n - 1 - (k - m + 1);
+            const nsteps: usize = if (k < @min(m, n)) k + 1 else k + 1 - (2 * k - (n - 1 + m - 1));
+            var v: usize = 0;
+            var key1: u32 = 0;
+            var key2: u24 = 0;
+            while (v < nsteps and v < 3) : ({
+                i -|= 1;
+                j -|= 1;
+                v += 1;
+            }) {
+                key1 = key1 << 8 | image[i][j];
+                key2 = key2 << 8 | image[i][j];
+            }
+            if (key2 == part2_pattern1 or key2 == part2_pattern2) {
+                marked_image[i + 2][j + 2] += 1;
+            }
+            while (v < nsteps) : ({
+                i -|= 1;
+                j -|= 1;
+                v += 1;
+            }) {
+                key1 = key1 << 8 | image[i][j];
+                key2 = key2 << 8 | image[i][j];
+                if (key1 == part1_pattern1 or key1 == part1_pattern2) {
+                    xmas_count1 += 1;
+                }
+                if (key2 == part2_pattern1 or key2 == part2_pattern2) {
+                    marked_image[i + 1][j + 1] += 1;
+                }
+            }
+        }
+    }
+
+    for (marked_image) |mrow| {
+        for (mrow) |val| {
+            if (val == 2) {
+                xmas_count2 += 1;
+            }
+        }
+    }
+
+    print("Part 1: {d}\n", .{xmas_count1});
+    print("Part 2: {d}\n", .{xmas_count2});
+}
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
