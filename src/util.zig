993317091da160ac964b58a3a803b3591b221f10
polykernel <81340136+polykernel@users.noreply.github.com>
2024-12-06 14:23:21 -0500

    feat: add day06 solution
    Comments: It took an embrassingly amount of time to realize
    that if an obstacle placement has been checked at a prior time when
    walking along the path, then it does not need to be checked again.

--- src/util.zig
+++ src/util.zig
@@ -4,12 +4,41 @@ const List = std.ArrayList;
 const Map = std.AutoHashMap;
 const StrMap = std.StringHashMap;
 const BitSet = std.DynamicBitSet;
+const IntegerBitSet = std.bit_set.IntegerBitSet;
 const Str = []const u8;
 
 var gpa_impl = std.heap.GeneralPurposeAllocator(.{}){};
 pub const gpa = gpa_impl.allocator();
 
 // Add utility functions here
+pub fn Pair(T1: type, T2: type) type {
+    return std.meta.Tuple(&.{ T1, T2 });
+}
+
+pub fn findLastSet(comptime StaticBitSetType: type, bit_set: StaticBitSetType) ?usize {
+    const is_array_bit_set = comptime startsWith(u8, @typeName(StaticBitSetType), "bit_set.ArrayBitSet");
+    const is_int_bit_set = comptime startsWith(u8, @typeName(StaticBitSetType), "bit_set.IntegerBitSet");
+    if (is_int_bit_set) {
+        const mask = bit_set.mask;
+        if (mask == 0) return null;
+        return StaticBitSetType.bit_length - 1 - @clz(mask);
+    } else if (is_array_bit_set) {
+        var offset: usize = 0;
+        const bit_size = @bitSizeOf(StaticBitSetType.MaskInt);
+        const mask = blk: {
+            var i = bit_set.masks.len;
+            while (i > 0) : (i -= 1) {
+                const m = bit_set.masks[i - 1];
+                if (m != 0) break :blk m;
+                offset += bit_size;
+            } else return null;
+        };
+        const idx = offset + @clz(mask);
+        return StaticBitSetType.bit_length - 1 - idx;
+    } else {
+        @compileError("Unimplemented");
+    }
+}
 
 // Useful stdlib functions
 const tokenizeAny = std.mem.tokenizeAny;
@@ -27,6 +56,7 @@ const lastIndexOfStr = std.mem.lastIndexOfLinear;
 const trim = std.mem.trim;
 const sliceMin = std.mem.min;
 const sliceMax = std.mem.max;
+const startsWith = std.mem.startsWith;
 
 const parseInt = std.fmt.parseInt;
 const parseFloat = std.fmt.parseFloat;
